000fccfa8195e86a4a05af5c72e03cb69c4b92c9
Skadic <etieti22@gmail.com>
2019-11-10 14:56:18 +0100

    Finish conversion script and add converted data to the cpu

--- scripts/lookup_conversion.py
+++ scripts/lookup_conversion.py
@@ -2,13 +2,15 @@ import re
 
 pattern = re.compile(r'{\s*"(\S+?)",\s*&a::(\S+?),\s*&a::(\S+?),\s*(\S+)\s*}')
 
+inFile = open("scripts/cpp_lookup_table.txt")
+lines = inFile.readlines()
+inFile.close()
 
-with open("scripts/cpp_lookup_table.txt") as inFile:
-    lines = inFile.readlines()
-
-    for line in lines:
-        matches = pattern.findall()
-        with open("scripts/out.txt", "a") as outFile:
-            for match in matches:
-                print('Instruction::new("', match[0], '", ', )
+for line in lines:
+    matches = pattern.findall(line)
+    with open("scripts/out.txt", "a") as outFile:
+        outFile.write('vec![')
+        for match in matches:
+            outFile.write('Instruction::new("%s", Self::%s, Self::%s, %s), ' % (match[0], match[1], match[2], match[3]))
+        outFile.write('],\n')
     
